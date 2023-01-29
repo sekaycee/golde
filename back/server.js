ec37ea9ae3640e474d34fe9cc5781cdcd8c5c367
Kay Cee <se.kaycee@gmail.com>
2023-01-30 00:49:22 +0100

    remove / in server.js

--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3003
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
