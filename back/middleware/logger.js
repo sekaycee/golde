7d99ad6c83d43d04acb272bc63c93e32e37d0fdb
Kay Cee <se.kaycee@gmail.com>
2023-01-30 18:25:12 +0100

    debug logger.js

--- back/middleware/logger.js
+++ back/middleware/logger.js
@@ -1,5 +1,5 @@
 const { format } = require('date-fns')
-const { v4: uuud } = require('uuid')
+const { v4: uuid } = require('uuid')
 const fs = require('fs')
 const fsPromises = require('fs').promises
 const path = require('path')
