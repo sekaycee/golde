09be2ef655199a4e5f24b3f8ffb7ecbda4bb09bc
Kay Cee <se.kaycee@gmail.com>
2023-01-30 18:17:32 +0100

    add logger.js

--- back/server.js
+++ back/server.js
@@ -3,6 +3,8 @@ const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 3003
 
+app.use(express.json())
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
