7bafa4a400d6e151638fe031849b2d0133bb304f
Kay Cee <se.kaycee@gmail.com>
2023-01-30 21:32:18 +0100

    add cookie-parser

--- back/server.js
+++ back/server.js
@@ -3,11 +3,14 @@ const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3003
 
 app.use(logger)
 
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
