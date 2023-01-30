836215c331eb7a514324d9871b41594f5ee815d4
Kay Cee <se.kaycee@gmail.com>
2023-01-30 18:24:04 +0100

    add logger to server.js

--- back/server.js
+++ back/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
 const PORT = process.env.PORT || 3003
+
+app.use(logger)
 
 app.use(express.json())
 
