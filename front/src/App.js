631294fd04bccab28e989f19dd33e747f6ab4f3f
Kay Cee <se.kaycee@gmail.com>
2023-02-17 23:17:18 +0100

    add the public-facing side of the front

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import Public from './components/Public'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route index element={<Public />} />
       </Route>
     </Routes>
   );
---
+++ front/src/components/Public.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+  const content = (
+    <section className="public">
+      <header></header>
+      <main></main>
+      <footer></footer>
+    </section>
+  )
+  return content
+}
+
+export default Public
