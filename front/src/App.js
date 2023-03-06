bfbc98547f289d8dfe6f14747db276ff04817421
Kay Cee <se.kaycee@gmail.com>
2023-03-06 09:52:39 +0100

    add feature: UsersList and ProductsList

--- front/src/App.js
+++ front/src/App.js
@@ -4,6 +4,8 @@ import Public from './components/Public'
 import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
 import Welcome from './features/auth/Welcome'
+import UsersList from './features/users/UsersList'
+import ProductsList from './features/products/ProductsList'
 
 function App() {
   return (
@@ -14,6 +16,14 @@ function App() {
 
         <Route path="dash" element={<DashLayout />}>
 	  <Route index element={<Welcome />} />
+
+	  <Route path="users">
+	    <Route index element={<UsersList />} />
+	  <Route/>
+	  
+	  <Route path="products">
+	    <Route index element={<ProductsList />} />
+	  <Route/>
         </Route>
       </Route>
     </Routes>
---
+++ front/src/features/products/ProductsList.js
@@ -0,0 +1,7 @@
+const ProductsList = () => {
+  return (
+    <h1>Products List</h1>
+  )
+}
+
+export default ProductsList
