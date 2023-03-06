6f99328f8fca3efcff422d3a3f2e8d1495204761
Kay Cee <se.kaycee@gmail.com>
2023-03-06 09:34:16 +0100

    add feature: Welcome

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Public from './components/Public'
 import Login from './features/auth/Login'
 import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="login" element={<Login />} />
 
         <Route path="dash" element={<DashLayout />}>
+	  <Route index element={<Welcome />} />
         </Route>
       </Route>
     </Routes>
